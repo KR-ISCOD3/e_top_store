5c77e7c6fe2c44971d5bebb941a4cdbf983173ec
thetsinut <thet.sinut.2822@rupp.edu.kh>
2025-11-28 03:23:44 +0700

    Resolve merge conflict in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-// import 'screens/explore_screen.dart';
-// import 'screens/home_screen.dart';
-// import 'screens/favourite_screen.dart';
 import 'screens/payment_methods_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-/// Root app widget - open ExploreScreen directly.
+/// Root app widget - open PaymentMethodsScreen directly.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -16,9 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: ExploreScreen(),
-      // home: HomeScreen(),
-      // home: FavouriteScreen(),
       home: PaymentMethodsScreen(),
     );
   }
